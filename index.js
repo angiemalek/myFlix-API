672c5efd979d39b0e70b529a5c48590319d1a569
angiemalek <angiemalek@gmail.com>
2022-05-18 09:48:59 -0500

    Update index.js
    add GET route for movies endpoint

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const express = require("express");
 const app = express();
+
+let topMovies = [
+  {
+    title: "",
+    director: ""
+  },
+  {
+    title: "",
+    director: ""
+  },
+  {
+    title: "",
+    director: ""
+  },
+];
+
+//GET REQUESTS
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
+// LISTEN FOR REQUESTS
+app.listen(8080, () => {
+  console.log("App is listening to port 8080");
+});
