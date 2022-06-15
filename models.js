aa8cb655a5458c31834857a33305e648349a3595
angiemalek <angiemalek@gmail.com>
2022-06-15 15:45:18 -0500

    Update models.js
    create mongoose schema models

--- models.js
+++ models.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+
+let movieSchema = mongoose.Schema({
+  Title: {type: String, required: true},
+  Description: {type: String, required: true},
+  Genre: {
+    Name: String,
+    Description: String
+  },
+  Director: {
+    Name: String,
+    Bio: String
+  },
+  ImagePath: String,
+  Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+  Name: {type: String, required: true},
+  Password: {type: String, required: true},
+  Email: {type: String, required: true},
+  Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
+});
+
+module.exports.Movie = Movie;
+module.exports.User = User;
