e438048a5f42093b02baff5935ca1c62f494334f
angiemalek <angiemalek@gmail.com>
2022-07-01 15:46:30 -0500

    Create auth.js
    create login endpoint with JWT token/HTTP authentication

---
+++ auth.js
@@ -0,0 +1,36 @@
+const jwtSecret = "your_jwt_secret"; // This needs to be the same key as in JWT Strategy
+
+const jwt = require("jsonwebtoken"),
+  passport = require("passport");
+
+require("./passport");  // Local passport file
+
+
+let generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.Username, // The username you are encoding in JWT
+    expiresIn: "7d", //Specifies token expires in 7 days
+    algorithm: "HS256"  //Algorithm used to "sign" the values of the JWT
+  });
+}
+
+/* POST Login */
+module.exports = (router) => {
+  router.post("./login", (req, res) => {
+    passport.authenticate("local", {session: false}, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: "something is not right",
+          user: user
+        });
+      }
+      req.login(user, {session: false}, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({user, token});
+      });
+    })(req, res);
+  });
+}
