58fc7a0d8e327f391402605dbdb2d4e23ce36ddd
angiemalek <angiemalek@gmail.com>
2022-07-08 15:36:42 -0500

    Update index.js
    validating data user endpoint

--- index.js
+++ index.js
@@ -109,7 +109,21 @@ app.get("/movies/Director/:Name", passport.authenticate("jwt", {session: false})
 });
 
 // CREATE(POST) NEW USER MONGOOSE
-app.post("/users", (req, res) => {
+app.post("/users",
+  [
+    check("Username", "Username is required").isLength({min: 5}),
+    check("Username", "Username contains non alphanumeric characters-not allowed").isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail()
+  ], (req, res) => {
+
+    //checks validation object for errors
+    let errors = validationResult(req),
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    
   let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })
     .then((user) => {
